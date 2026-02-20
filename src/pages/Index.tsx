7b6762f38af2e693b8929caba5d0da7e03407678
jovensaprendizeslatem-alt <jovensaprendizeslatem@gmail.com>
2026-02-20 13:00:53 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -102,7 +102,7 @@ const Index = () => {
     <div className="min-h-screen bg-background font-sans">
       {/* Fixed Logo */}
       <div className="fixed top-4 left-4 z-50">
-        <img src={logoLatem} alt="LATEM Logo" className="w-25 h-25" />
+        <img src={logoLatem} alt="LATEM Logo" className="w-18 h-18" />
       </div>
 
       {/* Hero Section */}
