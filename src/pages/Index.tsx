bc0af50fe64fb5e9b84fe77a3f619b1f30f33ef0
jovensaprendizeslatem-alt <jovensaprendizeslatem@gmail.com>
2026-02-20 14:04:28 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -102,7 +102,7 @@ const Index = () => {
     <div className="min-h-screen bg-background font-sans">
       {/* Fixed Logo */}
       <div className="fixed top-4 left-4 z-50">
-        <div className="w-24 h-24 flex items-center justify-center"> 
+        <div className="w-40 h-40 flex items-center justify-center"> 
   <img 
     src="/Logo.png"  
     alt="Logo da Empresa" 
